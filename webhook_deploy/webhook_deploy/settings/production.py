72ce0e7985cf5cc935ccbce2a858f4dbddb7f80f
umezaki <bungoume3@yahoo.co.jp>
2015-02-18 23:58:14 +0900

    django-admin startproject webhook_deploy

---
+++ webhook_deploy/webhook_deploy/settings/production.py
@@ -0,0 +1,17 @@
+from webhook_deploy.settings import *  # NOQA
+import dj_database_url
+
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+DATABASES = {
+    'default':
+        dj_database_url.config(default=os.environ['DATABASE_URL'])
+}
---
+++ webhook_deploy/webhook_deploy/settings/test.py
@@ -0,0 +1,9 @@
+from webhook_deploy.settings import *  # NOQA
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
