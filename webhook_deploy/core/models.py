36e615e89563b46b8497d4e22ad102e13374b6f6
umezaki <bungoume3@yahoo.co.jp>
2015-02-28 16:08:32 +0900

    update core models

--- webhook_deploy/core/models.py
+++ webhook_deploy/core/models.py
@@ -14,17 +14,31 @@ class Repository(models.Model):
     # Secret Key
     secret = models.CharField(max_length=191, db_index=True)
 
+    def __str__(self):
+        return '{}: {}'.format(self.hub, self.full_name)
+
 
 class DeploySetting(models.Model):
     repository = models.ForeignKey(Repository)
     branch = models.CharField(max_length=191)
     command = models.TextField()
 
+    def __str__(self):
+        return '{}: {}'.format(self.repository, self.branch)
+
 
 class HookLog(models.Model):
     data = JSONField()
-    created_at = models.DateTimeField("レコード作成日時", auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.created_at, self.data.get('path'))
 
 
 class DeployLog(models.Model):
     log = models.TextField()
+    return_code = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.created_at, self.return_code)
